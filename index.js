1dc5c054ea174bf24b8e9e80cb4287846eea9eff
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-05-17 05:49:36 -0700

    final changes

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ function newImage(url, left, bottom_)
     document.body.append(object)
     return object 
 }
+let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
 
 // creatine the function for the item. 
 
